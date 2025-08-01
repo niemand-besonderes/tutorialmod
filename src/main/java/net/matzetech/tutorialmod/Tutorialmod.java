2d2a0a1e8943a4c0028f3137e7036e8be2484f4c
Matzetech <matze.b@mail.de>
2025-08-01 16:11:00 +0200

    Initial commit

---
+++ src/main/java/net/matzetech/tutorialmod/Tutorialmod.java
@@ -0,0 +1,15 @@
+package net.matzetech.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorialmod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/matzetech/tutorialmod/TutorialmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.matzetech.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
